201a0df8407b36e9deb0477a3811daadced67b3e
awan1701 <jefriwijaya@gmail.com>
2023-06-13 23:11:25 +0700

    Final Project PMO

---
+++ android/app/src/main/kotlin/com/example/marisanif21a/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.marisanif21a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/awal.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class awal extends StatefulWidget {
+  const awal({super.key});
+
+  @override
+  State<awal> createState() => _awalState();
+}
+
+class _awalState extends State<awal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
