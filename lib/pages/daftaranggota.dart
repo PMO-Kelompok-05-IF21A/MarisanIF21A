263e397be83ebb540fc5c1b7371694422e527dce
BagasAjiF <bagasaji7890@gmail.com>
2023-07-19 20:17:59 +0700

    Memperbaiki Kesalahan

--- lib/pages/daftaranggota.dart
+++ lib/pages/daftaranggota.dart
@@ -4,7 +4,7 @@ import 'package:marisanif21a/pages/bayar.dart';
 import 'package:marisanif21a/pages/home.dart';
 import 'package:marisanif21a/pages/recordbayar.dart';
 import 'package:marisanif21a/pages/tambahanggota.dart';
-// ini Harusnya perbaikan
+// ini Harusnya perbaikan Hall
 class DaftarAnggota extends StatefulWidget {
   const DaftarAnggota({Key? key}) : super(key: key);
 
